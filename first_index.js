77c562717a7a3440f536034cf9d7488f87b4c9bd
devcodes21 <72709674+devcodes21@users.noreply.github.com>
2023-01-31 16:55:53 +0530

    Add files via upload

---
+++ first_index.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("#new").click(function(){
+        alert("Hello World");
+    })
+})
+$(document).ready(function(){
+    $("#neww").click(function(){
+        alert("Noo mat kar");
+    })
+})
