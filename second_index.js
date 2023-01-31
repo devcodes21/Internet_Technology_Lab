99baa27c5b158b039ec2eb6e4d192ca1c720f694
devcodes21 <72709674+devcodes21@users.noreply.github.com>
2023-01-31 16:56:13 +0530

    Add files via upload

---
+++ second_index.js
@@ -0,0 +1,12 @@
+alert("Hello World");
+$("#table_check").css({ "border-color": "black", "font-size": "100%" });
+$(document).ready(function () {
+  $("#btn").click(function () {
+    $("#table_check").css({ "margin-left": "300px" });
+  });
+});
+$("#btn").click(function () {
+  $("#table_check").css({ "margin-left": "300px" });
+  $("#first_image").css({ "margin-left": "300px" });
+});
+// $('#table_check').css({"margin-left":"300px"});
